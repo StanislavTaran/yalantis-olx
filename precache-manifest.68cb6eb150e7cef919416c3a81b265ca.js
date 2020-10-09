7a3cef0fe2b1892f112bf815f40fbd0612894045
Stanislav Taran <stastaransa1nt@gmail.com>
2020-10-09 13:08:26 +0300

    Updates

---
+++ precache-manifest.68cb6eb150e7cef919416c3a81b265ca.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c33fac3665909099bc7c9e2680bf93a",
+    "url": "/yalantis-olx/index.html"
+  },
+  {
+    "revision": "7833b0906ff564db6a45",
+    "url": "/yalantis-olx/static/css/5.b42794fd.chunk.css"
+  },
+  {
+    "revision": "edee3ec27526a49007bd",
+    "url": "/yalantis-olx/static/css/cart-page.b62948f8.chunk.css"
+  },
+  {
+    "revision": "9095a7c7804df5874a94",
+    "url": "/yalantis-olx/static/css/main.4e652f31.chunk.css"
+  },
+  {
+    "revision": "7d406b8f013b2b02aa26",
+    "url": "/yalantis-olx/static/css/products-page.5ff6e4e6.chunk.css"
+  },
+  {
+    "revision": "7833b0906ff564db6a45",
+    "url": "/yalantis-olx/static/js/5.e3d7b187.chunk.js"
+  },
+  {
+    "revision": "be3ae91867bd4c806afeb90c76d5e54f",
+    "url": "/yalantis-olx/static/js/5.e3d7b187.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edee3ec27526a49007bd",
+    "url": "/yalantis-olx/static/js/cart-page.83d32c4c.chunk.js"
+  },
+  {
+    "revision": "9095a7c7804df5874a94",
+    "url": "/yalantis-olx/static/js/main.51b1c8de.chunk.js"
+  },
+  {
+    "revision": "03b1ec1b6aa49bb55337",
+    "url": "/yalantis-olx/static/js/own-products-page.96ae7ffb.chunk.js"
+  },
+  {
+    "revision": "7d406b8f013b2b02aa26",
+    "url": "/yalantis-olx/static/js/products-page.eb740571.chunk.js"
+  },
+  {
+    "revision": "6b2189a86ceb8cdd7a14",
+    "url": "/yalantis-olx/static/js/runtime-main.efa3482d.js"
+  },
+  {
+    "revision": "59a2698d6bf887641231d16043225011",
+    "url": "/yalantis-olx/static/media/briefcase.59a2698d.svg"
+  },
+  {
+    "revision": "4208f09fac1fdc819aaa2bb4c98e9f31",
+    "url": "/yalantis-olx/static/media/cart.4208f09f.svg"
+  },
+  {
+    "revision": "ffdce1cac23753e5cd27cdda540ca6bb",
+    "url": "/yalantis-olx/static/media/default-avatar.ffdce1ca.jpg"
+  },
+  {
+    "revision": "dd4255d5b7c387447cf3208dbb3c236c",
+    "url": "/yalantis-olx/static/media/filters.dd4255d5.svg"
+  },
+  {
+    "revision": "029311036039d29999784277552b9ea8",
+    "url": "/yalantis-olx/static/media/nextArrow.02931103.svg"
+  },
+  {
+    "revision": "dc7219c0d6e544d3d137f02403ea94d8",
+    "url": "/yalantis-olx/static/media/plus.dc7219c0.svg"
+  },
+  {
+    "revision": "6775c9f87e0a732132d080cc53dae289",
+    "url": "/yalantis-olx/static/media/prevArrow.6775c9f8.svg"
+  }
+]);
